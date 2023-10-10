d24f0e92364278ffe5ca37eac81872eced3ebc64
farag-dev <faragabouelezz55@gmail.com>
2023-10-10 15:30:50 +0300

    first commit

---
+++ meals.sql
@@ -0,0 +1,127 @@
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Big Mac','Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun.',29.99,null,0,'burgers','meal','p1.jpeg');
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Double Quarter with Cheese','Each Double Quarter Pounder with Cheese features two quarter pound* 100% fresh beef burger patties that are hot, deliciously juicy and cooked when you order.',39.99,null,0,'burgers','meal','p2.jpeg');
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Quarter with Cheese Deluxe','Quarter with Cheese Deluxe is a fresh take on a Quarter Pounder® classic burger. Crisp shredded lettuce and three Roma tomato slices',35.99,null,0,'burgers','meal','p3.jpeg');
+
+
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Cheeseburger','Our simple, classic cheeseburger begins with a 100% pure beef burger seasoned with just a pinch of salt and pepper.',15.99,null,0,'burgers','meal','p4.jpeg');
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Double Cheeseburger','Double Cheeseburger features two 100% pure beef burger patties seasoned with just a pinch of salt and pepper.',25.99,null,0,'burgers','meal','p5.jpeg');
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Chicken Sandwich','It’s a classic for a reason. Savor the satisfying crunch of our juicy chicken patty, topped with shredded lettuce and just the right amount of creamy mayonnaise.',25.99,null,0,'burgers','meal','p6.jpeg');
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Spicy Crispy Chicken Sandwich','The Spicy Deluxe Crispy Chicken is big on everything, including heat. Our southern-style fried chicken fillet on a potato roll.',35.99,null,0,'chicken','meal','p7.jpeg');
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Crispy Chicken Sandwich','Crispy Chicken Sandwich is a southern style fried chicken sandwich thats crispy, juicy and tender perfection.',37.99,null,0,'chicken','meal','p8.jpeg');
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Chicken Sandwich Crispy Bun','With our Spicy Pepper Sauce topping the southern style fried chicken fillet on a toasted potato roll, this sandwich was made for those who like it crispy, juicy, tender and hot.',27.99,null,0,'chicken','meal','p9.jpeg');
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Filet-O-Fish','Filet-O-Fish 380 Cal.380 Cal. Dive into our wild-caught Filet-O-Fish! This fish sandwich has fish sourced from sustainably managed fisheries.',27.99,null,0,'chicken','meal','p10.jpeg');
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Chicken Nuggets','Our tender, juicy Chicken Nuggets are made with 100% white meat chicken and no artificial colors, flavors or preservatives. .',27.99,null,0,'chicken','meal','p11.jpeg');
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Plain Chicken Sandwich','It’s a classic for a reason. Savor the satisfying crunch of our juicy chicken patty, topped with shredded lettuce and just the right amount of creamy mayonnaise.',57.99,null,0,'chicken','meal','p12.jpeg');
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Coca Cola','Coca-Cola is a refreshing soda option that complements all of your menu favorites. Have you ever wondered, ',7.99,null,0,'beverages','drink','p13.jpeg');
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Sprite','Sprite is a delicious lemon-lime fountain drink and is available in sizes extra small, small, medium, and large.',7.99,null,0,'beverages','drink','p14.jpeg');
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Mango Pineapple Smoothie','The McDonald’s Mango Pineapple Smoothie recipe features the sweet combination of fruit juices and purees such as mango and pineapple.',8.99,null,0,'beverages','drink','p15.jpeg');
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Sausage McMuffin','Sausage Muffin recipe features a warm, freshly toasted English muffin, topped with a savory hot sausage patty and a slice of melty American cheese.',12.99,null,0,'breakfast','meal','p16.jpeg');
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('
+Sausage Muffin with Egg','Sausage Muffin with Egg features a savory hot sausage, a slice of melty American cheese, and a delicious freshly cracked egg all on a freshly toasted English muffin.',15.99,null,0,'breakfast','meal','p17.jpeg');
+
+
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('
+Sausage Biscuit','Sausage Biscuit is the perfect sausage breakfast sandwich, made with sizzling hot sausage on a warm buttermilk biscuit.',13.99,null,0,'breakfast','meal','p18.jpeg');
+
+
+
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('
+Sausage Burrito','The Sausage Burrito is Breakfast Burrito and is loaded with fluffy scrambled egg, pork sausage, melty cheese, green chiles and onion! Its wrapped in a soft tortilla.',16.99,null,0,'breakfast','meal','p19.jpeg');
+
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('
+Hash Browns','Our Hash Browns are deliciously tasty and perfectly crispy. These shredded potato hash brown patties are prepared so they’re fluffy on the inside and crispy and toasty on the outside.',9.99,null,0,'breakfast','meal','p20.jpeg');
+
+
+
+
+INSERT INTO `products`(`name`, `description`, `price`, `sale_price`, `quantity`, `category`, `type`, `image`) VALUES ('Fruit & Maple Oatmeal','Our Fruit and Maple Oatmeal has two full servings of whole-grain oats with a touch of cream and brown sugar.',19.99,null,0,'breakfast','meal','p21.jpeg');
